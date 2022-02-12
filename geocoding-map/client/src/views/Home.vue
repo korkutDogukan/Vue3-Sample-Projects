bdadaa493b32b76bc89d911c90472206c0294740
Doğukan Korkut <doguuu1998@hotmail.com>
2022-02-12 17:03:19 +0300

    Map uygulaması için gerekli kurulumlar yapıldı

---
+++ geocoding-map/client/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
+  </div>
+</template>
+
+<script>
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
+export default {
+  name: "Home",
+  components: {},
+  setup() {
+    let map;
+    onMounted(() => {
+      //init map
+      map = leaflet.map("map").setView([51.505, -0.09], 13);
+
+      //add tile layer
+      leaflet
+        .tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: process.env.VUE_APP_API_KEY,
+          }
+        )
+        .addTo(map);
+    });
+
+    const coords = ref(null);
+    const fetchCoords = ref(null);
+    const geoMarker = ref(null);
+  },
+};
+</script>
---
+++ geocoding-map/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+// init express 
+const app = express();
+
+// enable cors 
+app.use(cors());
+
+//routes
+app.use('/api/search/', require('./routes/searchResults.js'));
+
+app.listen(PORT, () => console.log(`app has started on port: ${PORT}`));
