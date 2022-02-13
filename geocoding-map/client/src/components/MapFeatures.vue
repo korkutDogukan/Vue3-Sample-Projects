5c67071d640c8c6d635ac0e6a0eee68ffd2b59ac
Doğukan Korkut <doguuu1998@hotmail.com>
2022-02-13 15:01:51 +0300

    Güncel konum alma, storage a ekleme işlemleri yapıldı

---
+++ geocoding-map/client/src/components/MapFeatures.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="
+      w-full
+      md:w-auto
+      absolute
+      md:top-[40px] md:left-[60px]
+      z-[2]
+      flex
+      gap-4
+      px-6
+      py-8
+      md:px-0 md:py-0
+      bg-transparent
+    "
+  >
+    <!-- Geolocation -->
+    <div
+      class="px-4 bg-white flex items-center shadow-md rounded-md min-h-[45px]"
+    >
+      <i class="fas fa-location-arrow 'text-slate-600' text-[18px]"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- geocoding-map/client/src/views/Home.vue
+++ geocoding-map/client/src/views/Home.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="h-screen relative">
-    <div id="map" class="h-full z-[1]"></div>
+    <GeoErrorModal
+      v-if="geoError"
+      :geoErrorMsg="geoErrorMsg"
+      @closeGeoError="closeGeoError"
+    />
+    <MapFeatures
+      class="w-full md:w-auto absolute md:top-[40px] md:left-[60px] z-[2]"
+    />
+    <div id="mapid" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script>
 import leaflet from "leaflet";
 import { onMounted, ref } from "vue";
+import GeoErrorModal from "../components/GeoErrorModal.vue";
+import MapFeatures from "../components/MapFeatures.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { GeoErrorModal, MapFeatures },
   setup() {
     let map;
     onMounted(() => {
       //init map
-      map = leaflet.map("map").setView([51.505, -0.09], 13);
+      map = leaflet.map("mapid").setView([51.505, -0.09], 13);
 
       //add tile layer
       leaflet
@@ -31,11 +41,82 @@ export default {
           }
         )
         .addTo(map);
+
+      getGeolocation();
     });
 
     const coords = ref(null);
     const fetchCoords = ref(null);
     const geoMarker = ref(null);
+    const geoError = ref(null);
+    const geoErrorMsg = ref(null);
+
+    const getGeolocation = () => {
+      // check to see if we have coods in session sotrage
+      if (sessionStorage.getItem("coords")) {
+        coords.value = JSON.parse(sessionStorage.getItem("coords"));
+        plotGeoLocation(coords.value);
+        return;
+      }
+
+      // else get coords from geolocation API
+      fetchCoords.value = true;
+      navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+    };
+
+    const setCoords = (pos) => {
+      // stop fetching
+      fetchCoords.value = null;
+
+      // set coords in session storage
+      const setSessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+      sessionStorage.setItem("coords", JSON.stringify(setSessionCoords));
+
+      // set ref coords value
+      coords.value = setSessionCoords;
+
+      plotGeoLocation(coords.value);
+    };
+
+    const getLocError = (error) => {
+      // stop fetching coords
+      fetchCoords.value = null;
+      geoError.value = true;
+      geoErrorMsg.value = error.message;
+    };
+
+    const plotGeoLocation = (coords) => {
+      // create custom marker
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/map-marker-red.svg"),
+        iconSize: [35, 35],
+      });
+
+      // create new marker with coords and custom marker
+      geoMarker.value = leaflet
+        .marker([coords.lat, coords.lng], { icon: customMarker })
+        .addTo(map);
+
+      // set map view to current location
+      map.setView([coords.lat, coords.lng], 10);
+    };
+
+    const closeGeoError = () => {
+      geoErrorMsg.value = null;
+      geoError.value = null;
+    };
+
+    return {
+      geoError,
+      closeGeoError,
+      geoErrorMsg,
+      fetchCoords,
+      coords,
+      getGeolocation,
+    };
   },
 };
 </script>
