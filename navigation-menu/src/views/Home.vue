8d8cff7dd3cc67ff3b97e7deddcb76466ab2f2e7
Doğukan Korkut <doguuu1998@hotmail.com>
2022-02-16 12:12:53 +0300

    Responsive navigation uygulaması eklendi

---
+++ navigation-menu/src/views/Home.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-text container">
+        <h4>Vue Responsive Nav Menu</h4>
+        <hr />
+        <h2>
+          With Vue <br />
+          Animations
+        </h2>
+        <hr />
+      </div>
+    </section>
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quasi,
+        quae voluptate quia ipsa quibusdam assumenda cupiditate. Vero esse
+        deleniti consequatur assumenda illo itaque ratione quam possimus. Ad
+        incidunt architecto rerum, adipisci consectetur hic tempore ea quam
+        voluptatum dolor ipsam non dignissimos modi nam fugiat quis in
+        accusamus? Ab libero quo adipisci consequuntur ut quam aliquam similique
+        accusamus earum inventore praesentium maiores dolore error placeat sunt
+        itaque facere nihil mollitia, autem ex quidem et, obcaecati voluptatum?
+        Vitae eligendi aspernatur eveniet delectus amet cumque. Reiciendis
+        beatae laborum numquam perferendis rerum. Nemo molestias veniam dolorum
+        veritatis placeat praesentium quia explicabo magni optio reiciendis
+        natus doloribus voluptatum, pariatur quam doloremque nam corporis quae
+        similique amet exercitationem repellendus sed eos? Fuga, illo sed earum
+        sit eaque inventore porro quae dolorum est voluptate rerum illum aliquam
+        impedit alias recusandae, ullam nam, laboriosam consequatur dolorem
+        sequi dolor. Dignissimos similique nihil nostrum sit quod accusantium
+        aut vel aliquam voluptatem tenetur? Vitae voluptatum necessitatibus eos
+        fugiat culpa atque illo quibusdam, molestiae cupiditate quis quae hic,
+        sit, debitis voluptates adipisci quos voluptatem? Vel libero aspernatur
+        quia perferendis. Maxime, fuga ipsa, vitae necessitatibus consectetur
+        sapiente autem odio illum, impedit rem doloremque architecto. Esse
+        facere iusto mollitia modi natus a quia numquam dolor. Incidunt, vitae
+        tenetur? Suscipit, quos delectus molestiae perspiciatis obcaecati quasi
+        vitae dicta culpa. Quas quaerat enim non inventore laboriosam aperiam
+        sint repudiandae, sed, repellat adipisci nostrum, beatae expedita odio
+        ab tempore corrupti error. Iste aperiam iure beatae quia exercitationem
+        distinctio rerum error voluptatem eaque earum sit, facere corrupti!
+        Sequi quod nulla nesciunt sapiente ducimus soluta ratione saepe
+        mollitia, explicabo at aspernatur amet asperiores neque voluptate
+        voluptatum accusantium. Placeat illum ullam earum, dolores a quo
+        voluptates qui quod, aperiam, ratione dolorum modi blanditiis. Alias
+        quas doloribus distinctio. Libero aperiam dicta culpa eum animi
+        repellat, sint sed eius, hic ab nam beatae, eos magni consequatur
+        mollitia ipsa commodi tempore quo cupiditate doloremque unde
+        exercitationem quia maxime. Quis accusantium obcaecati non autem minima,
+        commodi iusto perspiciatis veniam porro est error illo inventore ab ea
+        saepe voluptatibus dignissimos. Ratione, est. Reprehenderit quae maxime
+        iusto, ea obcaecati magnam? Corrupti, libero voluptates. Consequatur
+        eligendi adipisci voluptates odit obcaecati ipsam cupiditate temporibus,
+        alias nam, blanditiis numquam mollitia, a deleniti quisquam commodi
+        impedit quam placeat harum maiores aut vel fuga! Quo magni minima
+        perferendis deleniti recusandae veniam, vitae blanditiis tenetur cum,
+        odit voluptate itaque temporibus laboriosam nulla adipisci perspiciatis
+        ullam enim obcaecati natus id architecto cumque nobis porro voluptatem?
+        Ullam rerum laboriosam iusto repudiandae dolore excepturi obcaecati
+        placeat natus veritatis in explicabo nostrum quibusdam facilis, a cumque
+        laudantium laborum labore adipisci, qui, quod libero eos fugit ab
+        quidem? Quos sunt, ratione beatae, sapiente nobis, doloribus debitis
+        accusamus tenetur quisquam modi optio provident quaerat? Nesciunt
+        voluptate blanditiis aspernatur odit sint deserunt, omnis veniam
+        doloremque, facilis eveniet adipisci minima consectetur excepturi
+        molestias, commodi vel sunt? Modi quaerat, aperiam hic error tenetur
+        nisi! Optio a beatae mollitia totam aspernatur debitis quo sed accusamus
+        fuga, ipsam hic. Amet assumenda, odit minima totam ducimus quam dolores
+        libero facere culpa voluptatum labore fuga similique. Recusandae commodi
+        id magnam iusto quod. Ipsa nulla beatae repellendus officiis modi ullam
+        excepturi debitis dignissimos reprehenderit autem accusantium vel
+        molestias tempore repudiandae eveniet, rerum amet explicabo voluptatibus
+        suscipit nobis! Hic eveniet libero aliquam. At quis quo minus id?
+        Suscipit debitis maiores inventore itaque unde ipsam, voluptatibus
+        accusantium facere pariatur laboriosam, perspiciatis nulla? Saepe quam
+        impedit deleniti nostrum error corrupti eum qui ratione delectus. Quo
+        consectetur odit quam dolores, culpa voluptates praesentium, modi
+        distinctio eos officia saepe suscipit consequuntur atque quia nihil
+        voluptatem quod cumque minus maxime enim alias reiciendis repellat
+        deleniti nam. Fuga eos in nisi totam voluptas dolorem reprehenderit
+        saepe repudiandae quidem ipsam delectus vero, dignissimos veniam
+        adipisci reiciendis! Alias quasi facere, dicta cum nobis nostrum eum
+        labore exercitationem quod quam corrupti praesentium repellendus libero
+        adipisci amet est eveniet dolores voluptates eligendi illo similique!
+        Pariatur necessitatibus, voluptatum, iure optio officiis ipsum
+        distinctio dolores nostrum nobis accusamus magni voluptates aliquam
+        accusantium laudantium quo! Iste enim, repellendus dolores vel quo
+        dolorum eum quis impedit velit, officia dolor! Eligendi, odit asperiores
+        vero quod, at aperiam nostrum dolore perspiciatis maiores recusandae
+        tempore ratione neque sequi laborum quisquam explicabo inventore! Modi
+        harum autem repudiandae quas veniam velit tempore laboriosam qui, dicta
+        ducimus, voluptates, quam vero alias. Illum ipsam est saepe dolorem
+        velit blanditiis dolorum voluptate. Et debitis ipsum voluptate quo
+        facere necessitatibus maxime vero, delectus corporis illo omnis possimus
+        numquam, iure similique ex ullam assumenda ad in, excepturi beatae
+        labore dolores molestias atque voluptatum? Maiores odit temporibus ea
+        possimus laudantium quae cumque dolorem blanditiis doloremque eligendi.
+        Doloribus alias omnis quisquam, amet nostrum itaque laborum distinctio,
+        labore laudantium reiciendis nesciunt iure blanditiis velit sed eos
+        recusandae aut doloremque! Nesciunt, assumenda, natus molestias corrupti
+        ad recusandae iure excepturi beatae deserunt dolore, minima adipisci
+        repellat ratione. Doloribus officia optio quam fugit inventore
+        consequatur nihil! Velit beatae harum, veniam consequuntur minima
+        ratione illum aut enim ex laborum. Ab, debitis. Eos perspiciatis quis
+        quasi doloremque fugit, aut inventore placeat repellat ullam autem,
+        necessitatibus accusantium, facilis ipsam deserunt cupiditate? Commodi
+        obcaecati, quaerat temporibus, nobis aperiam omnis incidunt voluptates
+        odit asperiores velit, doloremque dolore. Facere, provident reiciendis!
+        Iusto reiciendis maxime et inventore eligendi magni necessitatibus
+        excepturi velit atque fugit, impedit vel nam eaque similique odio?
+        Corporis minima deserunt aliquam nobis repellendus temporibus, incidunt
+        velit laudantium beatae magnam inventore iste repudiandae nostrum illum,
+        doloribus quod suscipit. Atque exercitationem facilis tempora harum
+        voluptate, autem laboriosam accusantium deserunt architecto, praesentium
+        vitae sed ullam et porro animi, totam est dolore saepe. Deleniti
+        pariatur accusamus ipsa. Illum odio repellendus a deleniti similique
+        tempora rerum recusandae dolorum enim, accusantium laudantium doloribus,
+        obcaecati reprehenderit vero est magnam laboriosam, eius error ab animi?
+        Eaque ipsa esse ipsam nulla soluta optio consequatur? Est eveniet
+        tempora officiis magnam, adipisci molestias ratione tempore iste officia
+        eius necessitatibus optio doloribus culpa praesentium! Nulla eligendi
+        sint assumenda dolore et necessitatibus reprehenderit culpa accusamus
+        consequatur quaerat autem aliquid nobis magni dignissimos optio,
+        officiis amet dolores. Accusantium esse consectetur possimus quibusdam
+        praesentium nulla autem. Quo voluptas molestias blanditiis nulla nobis
+        ea consequuntur facilis.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  background-image: url("../assets/hero-bg.png");
+  background-attachment: fixed;
+  position: relative;
+  height: 100vh;
+
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+  .hero-text {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #fff;
+
+    h4 {
+      text-transform: uppercase;
+      font-size: 22px;
+      padding-bottom: 4px;
+    }
+
+    h2 {
+      font-size: 50px;
+      @media (min-width: 550px) {
+        font-size: 80px;
+      }
+    }
+
+    hr:nth-child(2) {
+      max-width: 365px;
+      margin-bottom: 16px;
+    }
+    hr:nth-child(4) {
+      height: 6px;
+      background-color: #fff;
+      border: none;
+      max-width: 85px;
+      margin-top: 16px;
+    }
+  }
+}
+</style>
