8890ded460c064db8ef3a2baf1e83fcf17dac2c5
Doğukan Korkut <doguuu1998@hotmail.com>
2022-02-17 16:09:17 +0300

    modal example eklendi

---
+++ modal-example/src/views/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <Modal @close="toggleModal" :modalActive="modalActive">
+      <div class="modal-content">
+        <h1>This is a Modal Header</h1>
+        <p>This is a modal message</p>
+      </div>
+    </Modal>
+    <button type="button" @click="toggleModal">Open Modal</button>
+  </div>
+</template>
+
+<script setup>
+import Modal from "@/components/Modal.vue";
+import { ref } from "vue";
+const modalActive = ref(false);
+
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
+</script>
+
+
+<style lang="scss" scoped>
+.home {
+  background-color: rgba(0, 176, 234, 0.5);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .modal-content {
+    display: flex;
+    flex-direction: column;
+
+    h1,
+    p {
+      margin-bottom: 16px;
+    }
+
+    h1 {
+      font-size: 32px;
+    }
+
+    p {
+      font-size: 18px;
+    }
+  }
+}
+</style>
