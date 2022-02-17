6ec291675ded52a589e1558ec1143ee5ffcea7e0
Doğukan Korkut <doguuu1998@hotmail.com>
2022-02-17 14:35:13 +0300

    drag-drop app eklendi

---
+++ dropzone-app/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <h1>DropZone</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile" />
+    <span class="file-info">File: {{ dropzoneFile.name }}</span>
+  </div>
+</template>
+
+<script setup>
+// @ is an alias to /src
+import DropZone from "@/components/DropZone.vue";
+import { ref } from "vue";
+let dropzoneFile = ref("");
+
+const drop = (e) => {
+  dropzoneFile.value = e.dataTransfer.files[0];
+};
+
+const selectedFile = () => {
+  dropzoneFile.value = document.querySelector(".dropzoneFile").files[0];
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f1f1;
+  flex-direction: column;
+
+  h1 {
+    font-size: 40px;
+    margin-bottom: 32px;
+  }
+
+  .file-info {
+    margin-top: 32px;
+  }
+}
+</style>
